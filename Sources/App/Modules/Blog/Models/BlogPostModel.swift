94397cf1f7709e050e3cec46f2c63e680e8336a6
Artem Panasenko <52529151+PanArtemG@users.noreply.github.com>
2020-05-24 13:38:45 +0300

    :update_dlog_and_front_modules

--- Sources/App/Modules/Blog/Models/BlogPostModel.swift
+++ Sources/App/Modules/Blog/Models/BlogPostModel.swift
@@ -15,7 +15,6 @@ final class BlogPostModel: Model {
     struct FieldKeys {
         static var title: FieldKey { "title" }
         static var slug: FieldKey { "slug" }
-        static var imageKey: FieldKey { "image_key" }
         static var image: FieldKey { "image" }
         static var excerpt: FieldKey { "excerpt" }
         static var date: FieldKey { "date" }
@@ -26,7 +25,6 @@ final class BlogPostModel: Model {
     @ID() var id: UUID?
     @Field(key: FieldKeys.title) var title: String
     @Field(key: FieldKeys.slug) var slug: String
-    @Field(key: FieldKeys.imageKey) var imageKey: String?
     @Field(key: FieldKeys.image) var image: String
     @Field(key: FieldKeys.excerpt) var excerpt: String
     @Field(key: FieldKeys.date) var date: Date
@@ -38,7 +36,6 @@ final class BlogPostModel: Model {
     init(id: UUID? = nil,
          title: String,
          slug: String,
-         imageKey: String? = nil,
          image: String,
          excerpt: String,
          date: Date,
@@ -48,7 +45,6 @@ final class BlogPostModel: Model {
         self.id = id
         self.title = title
         self.slug = slug
-        self.imageKey = imageKey
         self.image = image
         self.excerpt = excerpt
         self.date = date
@@ -57,28 +53,27 @@ final class BlogPostModel: Model {
     }
 }
 
-//extension BlogPostModel: ViewContextRepresentable {
-//
-//    struct ViewContext: Encodable {
-//        var id: String
-//        var title: String
-//        var slug: String
-//        var image: String
-//        var excerpt: String
-//        var date: String
-//        var content: String
-//
-//        init(model: BlogPostModel) {
-//            self.id = model.id!.uuidString
-//            self.title = model.title
-//            self.slug = model.slug
-//            self.image = model.image
-//            self.excerpt = model.excerpt
-//            self.date = DateFormatter.year.string(from: model.date)
-//            self.content = model.content
-//        }
-//    }
-//
-//    var viewContext: ViewContext { .init(model: self) }
-//    var viewIdentifier: String { self.id!.uuidString }
-//}
+extension BlogPostModel {
+
+    struct ViewContext: Encodable {
+        var id: String
+        var title: String
+        var slug: String
+        var image: String
+        var excerpt: String
+        var date: String
+        var content: String
+
+        init(model: BlogPostModel) {
+            self.id = model.id!.uuidString
+            self.title = model.title
+            self.slug = model.slug
+            self.image = model.image
+            self.excerpt = model.excerpt
+            self.date = DateFormatter.year.string(from: model.date)
+            self.content = model.content
+        }
+    }
+
+    var viewContext: ViewContext { .init(model: self) }
+}
---
+++ Sources/App/Modules/User/Models/UserModule.swift
@@ -0,0 +1,8 @@
+//
+//  UserModel.swift
+//  App
+//
+//  Created by Artem Panasenko on 24.05.2020.
+//
+
+import Foundation
